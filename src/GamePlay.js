04f65ae0153cf49f706fa6fbdbd146e6ee429c31
cdemiguel <demiguelferrerocarlos@gmail.com>
2018-08-16 21:18:12 +0200

    added movement to horse

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,43 +1,71 @@
 GamePlayManager = {
+  init: function() {
+    // el juego escale en panalla
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+    // alinear al centro el juego
+    game.scale.pageAlignHorizontally = true
+    game.scale.pageAlignVertically = true
 
-    init: function() {
-        // el juego escale en panalla
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        // alinear al centro el juego
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
-    },
-    // carga recursos
-    preload: function() {
-        // cargar img background
-        game.load.image('background', '../assets/images/background.png');
-        // utilizar sprites se carga primero (utilizando diciendo a phaser que es un sprite)
-        // 84 la mitad en px de las dos partes, alto, 2 img en un sprite
-        game.load.spritesheet('horse', '../assets/images/horse.png', 84, 156, 2);
-    },
-    // utiliza recursos
-    create: function() {
-        // se printa la imagen posicion 0/0 y nombre de la img cargada
-        game.add.sprite(0, 0,'background');
-        // añadimos el caballo de mar
-        // ademas guardamos la instancia horse y que este dentro del objeto gameplaymanager
-        //podemos guardar instancia de nuestro sprite
-        this.horse = game.add.sprite(0, 0, 'horse');
-        this.horse.frame = 0;
-        this.horse.x = game.width/2;
-        this.horse.y = game.height/2;
-        // centrar el anchor del sprite en el centro// se añade un ancla en el centro del sprite
-        // como un punto, y todas las acciones del sprite se hacen a partir de ahi
-        this.horse.anchor.setTo(0,5);
-        // // podemos escalar tb 
-        // this.horse.scale.setTo(1,2)
-        // // podemos setear el alpha
-        // this.horse.alpha = 0; // invisible // 1 visible
-    },
-    // frame a frame llama al metodo
-    update: function() {
+    this.flagFirstMouseDown = false
+  },
+  // carga recursos
+  preload: function() {
+    // cargar img background
+    game.load.image("background", "../assets/images/background.png")
+    // utilizar sprites se carga primero (utilizando diciendo a phaser que es un sprite)
+    // 84 la mitad en px de las dos partes, alto, 2 img en un sprite
+    game.load.spritesheet("horse", "../assets/images/horse.png", 84, 156, 2)
+  },
+  // utiliza recursos
+  create: function() {
+    // se printa la imagen posicion 0/0 y nombre de la img cargada
+    game.add.sprite(0, 0, "background")
+    // añadimos el caballo de mar
+    // ademas guardamos la instancia horse y que este dentro del objeto gameplaymanager
+    //podemos guardar instancia de nuestro sprite
+    this.horse = game.add.sprite(0, 0, "horse")
+    this.horse.frame = 0
+    this.horse.x = game.width / 2
+    this.horse.y = game.height / 2
+    // centrar el anchor del sprite en el centro// se añade un ancla en el centro del sprite
+    // como un punto, y todas las acciones del sprite se hacen a partir de ahi
+    this.horse.anchor.setTo(0.5)
 
+    // // podemos escalar tb
+    // this.horse.scale.setTo(1,2)
+    // // podemos setear el alpha
+    // this.horse.alpha = 0; // invisible // 1 visible
+    // this.horse.angle = 0; // seteamo el angulo
+
+    // creamos un boton de inicio
+    game.input.onDown.add(this.onTap, this)
+  },
+  onTap: function() {
+    this.flagFirstMouseDown = true;
+  },
+  // frame a frame llama al metodo
+  update: function() {
+    if (this.flagFirstMouseDown) { // con el if hasta que no damos al botón el caballo no se mueve
+      // calcular posicion del mouse
+      var pointerX = game.input.x
+      var pointerY = game.input.y
+      // calcular distancia entre nuestro mouse y el ancla del caballo
+      var distX = pointerX - this.horse.x
+      var distY = pointerY - this.horse.y
+
+      // orientar el caballo hacia la izquierda o hacia la derecha, eso lo sabemos si la pos del caballo es mayor
+      // eso lo sabemos si la pos del caballo es mayor a la posicion del puntero
+      if (distX > 0) {
+        this.horse.scale.setTo(1, 1)
+      } else {
+        this.horse.scale.setTo(-1, 1)
+      }
+
+      //mover el horse se mueve en x solamente un porcentaje de la distancia para que vaya fluido
+      this.horse.x += distX * 0.02
+      this.horse.y += distY * 0.02
     }
+  }
 }
 
 // en phaser guardamos la instancia de nuestro juergo
@@ -45,5 +73,5 @@ GamePlayManager = {
 var game = new Phaser.Game(1136, 640, Phaser.CANVAS)
 
 // añadimos estado nombre de instancia y lo asignamos a un ombeto
-game.state.add('gameplay', GamePlayManager);
-game.state.start('gameplay');
+game.state.add("gameplay", GamePlayManager)
+game.state.start("gameplay")
